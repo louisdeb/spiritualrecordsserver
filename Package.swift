6a4f123c920c70388acf3af1d5b63c3236a79c69
Louis de Beaumont <debeaumont.louis@gmail.com>
2020-01-13 09:34:15 +0000

    Update for S3 4.0.0

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
       .package(url: "https://github.com/vapor/auth.git", from: "2.0.3"),
       .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0"),
       .package(url: "https://github.com/vapor-community/vapor-ext.git", from: "0.3.4"),
-      .package(url: "https://github.com/LiveUI/S3.git", from: "3.0.0")
+      .package(url: "https://github.com/LiveUI/S3.git", .revision("253053d5c046b6bd63771218049f62d37cf58653"))
     ],
     targets: [
         .target(name: "App", dependencies: ["Leaf", "Authentication", "FluentPostgreSQL", "Vapor", "VaporExt", "S3"]),
