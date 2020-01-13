503a0fd36f88e09d9aaeaa914197b0669a5c27af
Louis de Beaumont <debeaumont.louis@gmail.com>
2020-01-13 15:31:14 +0000

    Use fixed S3 framework

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
       .package(url: "https://github.com/vapor/auth.git", from: "2.0.4"),
       .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0"),
       .package(url: "https://github.com/vapor-community/vapor-ext.git", from: "0.3.4"),
-      .package(url: "https://github.com/LiveUI/S3.git", from: "3.0.0")
+      .package(url: "https://github.com/skelpo/s3-vapor3", .revision("a504056ac772aa41f2dd71da6ba1344909049592"))
     ],
     targets: [
         .target(name: "App", dependencies: ["Leaf", "Authentication", "FluentPostgreSQL", "Vapor", "VaporExt", "S3"]),
