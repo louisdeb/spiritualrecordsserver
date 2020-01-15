e1cb08278c4e73c8636c7e134b82bc05331e5a68
Louis de Beaumont <debeaumont.louis@gmail.com>
2020-01-15 14:29:24 +0000

    Remove imageURL columns from db

---
+++ Sources/App/Migrations/RemoveImageURLsFromArtist.swift
@@ -0,0 +1,23 @@
+//
+//  RemoveImageURLsFromArtist.swift
+//  App
+//
+//  Created by Louis de Beaumont on 15/01/2020.
+//
+
+import Vapor
+import FluentPostgreSQL
+
+struct RemoveImageURLsFromArtist: Migration {
+  typealias Database = PostgreSQLDatabase
+
+  static func prepare(on conn: PostgreSQLConnection) -> EventLoopFuture<Void> {
+    return Database.update(Artist.self, on: conn) { builder in
+      builder.deleteField(for: \Artist.imageURLs)
+    }
+  }
+
+  static func revert(on conn: PostgreSQLConnection) -> EventLoopFuture<Void> {
+    return conn.future()
+  }
+}
